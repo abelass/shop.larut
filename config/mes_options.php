2e1d84742a181bc879da2e66140957d1d8e45797
abelass <websolutions@mychacra.net>
2013-04-12 19:40:37 +0200

    premiere adaptations

---
+++ config/mes_options.php
@@ -0,0 +1,13 @@
+<?php
+$GLOBALS['toujours_paragrapher']=true;
+$GLOBALS['forcer_lang']=true;
+$GLOBALS['derniere_modif_invalide']=true;
+if (!defined('_DUREE_CACHE_DEFAUT')) define('_DUREE_CACHE_DEFAUT',86400);
+if (!defined('_DELAI_CACHE_resultats')) define('_DELAI_CACHE_resultats',600);
+$GLOBALS['quota_cache']=10;
+if (!defined('_LOGIN_TROP_COURT')) define('_LOGIN_TROP_COURT',4);
+if (!defined('_TRANCHES')) define('_TRANCHES',0);
+if (!defined('_INTERDIRE_COMPACTE_HEAD_ECRIRE')) define('_INTERDIRE_COMPACTE_HEAD_ECRIRE',true);
+$GLOBALS['filtrer_javascript']=1;
+
+?>
