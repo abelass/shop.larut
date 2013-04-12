d9ad2c294835554dc96bab14eced2fe5869c9c8b
abelass <websolutions@mychacra.net>
2013-04-12 16:31:59 +0200

    installation initiale

---
+++ squelettes/lang/local_en.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+
+    // G
+    'galerie_photos' => 'Galerie photos',
+    'une_photo' => 'Une Photo', 
+    
+    //L
+    'lire_la_suite'=>'Read More',
+    
+    //N
+    'news' => 'Just posted',   
+    
+    //S
+    'somme_produit' => 'Suma',
+    
+    //V
+    'voir_plus' => 'See more',        
+);
+
+?>
---
+++ squelettes/lang/local_fr.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+
+    // G
+    'galerie_photos' => 'Galerie photos',
+    'une_photo' => 'Une Photo', 
+    
+    //L
+    'lire_la_suite'=>'Lire la suite',
+    
+    //N
+    'news' => 'Nouveau sur ce site',    
+    
+    //S
+    'somme_produit' => 'Suma',
+    
+    //V
+    'voir_plus' => 'Voir d\'autres',    
+);
+
+?>
